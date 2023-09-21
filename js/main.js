7fb07e39d99c0b8c3b8c55dbac5955ebe51cb35c
Fransiskus Aldo <rochesterfransiskusaldo@gmail.com>
2023-09-21 01:09:50 -0400

    add file assets, html, and js file

---
+++ js/main.js
@@ -0,0 +1,62 @@
+$(".slider-holder").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 2,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 550,
+      settings: {
+        slidesToShow: 1,
+        dots: false,
+        autoplay: true,
+      },
+    },
+  ],
+});
+
+$(".slider-review").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  speed: 500,
+  arrows: false,
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 2,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 550,
+      settings: {
+        slidesToShow: 1,
+        dots: false,
+      },
+    },
+  ],
+});
